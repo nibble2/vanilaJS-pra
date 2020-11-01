1cc79c63e58e9c105190b9d126f4699329b909c7
nibble2 <nibblekk@gmail.com>
2020-11-02 05:10:57 +0900

    Fix function

--- todo-pra/index.js
+++ todo-pra/index.js
@@ -1,165 +1,102 @@
-const jsToDoForm = document.querySelector(".js-toDoForm"),
-    jsToDoInput = jsToDoForm.querySelector("input");
-const jsPendingList = document.querySelector(".js-pendingList");
-const jsFinishedList = document.querySelector(".js-finishedList");
+const pendingList = document.querySelector(".js-pendingList"),
+    finishedList = document.querySelector(".js-finishedList"),
+    form = document.querySelector(".js-toDoForm"),
+    input = form.querySelector("input");
 
-const PENDING_LS = "pending";
+const PENDING = "PENDING";
+const FINISHED = "FINISHED";
 
-const FINISH_LS = 'finished';
+let pendingTasks = [];
+let finishedTasks = [];
 
-let toDos = [];
-let finToDos = [];
+function saveState() {
+    localStorage.setItem(PENDING, JSON.stringify(pendingTasks));
+    localStorage.setItem(FINISHED, JSON.stringify(finishedTasks));
+}
 
-// find li
-function findParentNode(event) {
-    const btn = event.target;
-    const li = btn.parentNode;
+function buildGenericLi(text) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+
+    span.innerText = text;
+    deleteBtn.innerText = "❌";
+
+    li.append(span, deleteBtn);
 
     return li;
+
 }
 
-function backToDo(event) {
-    const li = findParentNode(event);
+function handleFinishClick(e) {
+    const li = e.target.parentNode;
 
-    const backText = li.childNodes[0].innerText;
-
-    paintToDo(backText);
-    deleteFinToDo(event);
-}
-
-function saveFinToDos() {
-    localStorage.setItem(FINISH_LS, JSON.stringify(finToDos));
-}
+    // pending화면에서 지우기
+    li.parentNode.removeChild(li);
 
 
-// finished 버튼 클릭 
-function finishToDo(event) {
-    const li = findParentNode(event);
+    //pending 로컬 스토리지 삭제
+    // const task = pendig 배열에서 아이디 일치하는 {id, text}를 가져온다 => findInPendig
+    // 내가 선택한 li.id와 task.id가 일치하면 삭제 => removeFormPending
 
-    const finText = li.childNodes[0].innerText;
-
-    paintFinToDo(finText);
-    deleteToDo(event);
-}
-
-function deleteFinToDo(event) {
-    const li = findParentNode(event);
-
-    jsFinishedList.removeChild(li);
-
-    const cleanToDos = finToDos.filter(function (toDo) {
-        return toDo.id !== parseInt(li.id);
-    });
-
-    finToDos = cleanToDos;
-
-    saveFinToDos();
-}
-
-function deleteToDo(event) {
-    const li = findParentNode(event);
-
-    jsPendingList.removeChild(li);
-
-    const cleanToDos = toDos.filter(function (toDo) {
-        return toDo.id !== parseInt(li.id);
-    });
-
-    toDos = cleanToDos;
-
-    saveToDos();
-}
-
-function saveToDos() {
-    localStorage.setItem(PENDING_LS, JSON.stringify(toDos));
-}
-
-function paintFinToDo(text) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
+    //finished 화면에 출력
+    const genericLi = buildGenericLi(text);
     const backBtn = document.createElement("button");
-    const delBtn = document.createElement("button");
-    const newId = finToDos.length + 1;
+    backBtn.innerText = "⏪";
 
+    genericLi.append(backBtn);
+    finishedList.append(li);
+    //finished 로컬 스토리지 추가
+    //새로운 task = {id, text}를 가져다가 그대로 넣음
 
-    backBtn.innerText = "↩️";
-    backBtn.addEventListener("click", backToDo);
-    delBtn.innerText = "❌";
-    delBtn.addEventListener("click", deleteFinToDo);
-    span.innerText = text;
-    li.id = newId;
+    // 상태 저장
+    //saveState();
+}
 
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    li.appendChild(backBtn);
-    jsFinishedList.appendChild(li);
+function paintPendingTask(task) {
+    const li = buildGenericLi(task.text);
+    const completBtn = document.createElement("button");
+    const newId = pendingTasks.length + 1;
+
+    completBtn.addEventListener("click", handleFinishClick);
+
+    completBtn.innerText = "✅";
+    li.append(completBtn);
+    //화면에 그림
+    pendingList.append(li);
 
     const toDoObj = {
         id: newId,
         text: text
     }
 
-    finToDos.push(toDoObj);
+    pendingTasks.push(toDoObj);
 
-    saveFinToDos();
+    saveState();
+
 }
 
-function paintToDo(text) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const finBtn = document.createElement("button");
-    const delBtn = document.createElement("button");
-    const newId = toDos.length + 1;
-
-    finBtn.innerText = "✅";
-    finBtn.addEventListener("click", finishToDo);
-    delBtn.innerText = "❌";
-    delBtn.addEventListener("click", deleteToDo);
-    span.innerText = text;
-    li.id = newId;
-
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    li.appendChild(finBtn);
-    jsPendingList.appendChild(li);
-
-    const toDoObj = {
-        id: newId,
-        text: text
-    }
-
-    toDos.push(toDoObj);
-
-    saveToDos();
+function getTaskObject(text) {
+    return {
+        id: String(Date.now()),
+        text
+    },
 }
 
-function inputSubmit(event) {
-    event.preventDefault();
-    const currentValue = jsToDoInput.value;
-    jsToDoInput.value = "";
-    paintToDo(currentValue);
+function handleSubmit(e) {
+    e.preventDefault();
+    const taskObj = getTaskObject(input.value);
+    paintPendingTask(taskObj);
+    input.value = "";
 }
 
-function loadToDos() {
-    const loadedPending = localStorage.getItem(PENDING_LS);
-    const loadedFinish = localStorage.getItem(FINISH_LS);
-
-    if (loadedPending !== null || loadedFinish !== null) {
-        const parsedPending = JSON.parse(loadedPending);
-        const parsedFinish = JSON.parse(loadedFinish)
-        parsedPending.forEach(function (toDo) {
-            paintToDo(toDo.text);
-        });
-
-        parsedFinish.forEach(function (toDo) {
-            paintFinToDo(toDo.text);
-        });
-    }
+function loadState() {
+    pendingList = JSON.parse(localStorage.getItem(PENDING)) || [];
+    findInFinished = JSON.parse(localStorage.getItem(FINISHED)) || [];
 }
 
 function init() {
-    loadToDos();
-    jsToDoForm.addEventListener("submit", inputSubmit);
+    form.addEventListener("submit", handleSubmit);
 }
 
 init();
